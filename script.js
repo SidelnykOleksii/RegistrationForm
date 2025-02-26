17dc2d2cb1b9bb654f6b1516e92cbbf07f13a916
oleksii_laptop <formovka4@ukr.net>
2025-02-26 17:28:13 +0200

    Initial commit

---
+++ script.js
@@ -0,0 +1,83 @@
+document
+  .getElementById("registerForm")
+  .addEventListener("submit", async function (event) {
+    event.preventDefault(); // Запобігаємо перезавантаженню сторінки
+
+    const name = document.getElementById("name").value;
+    const email = document.getElementById("email").value;
+    const password = document.getElementById("password").value;
+    const gender = document.querySelector('input[name="gender"]:checked')?.value;
+
+    if (!validatePassword(password)) return; // Виклик функції перевірки
+    if (!gender) {
+      alert("Please select a gender.");
+      return;
+    }
+
+    // Відправка запиту на сервер
+    const response = await fetch("http://localhost:3000/register", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ name, email, password, gender }),
+    });
+
+    const result = await response.json();
+
+    if (response.status === 409) {
+      alert("❌ Користувач з таким email вже існує!");
+    } else if (response.status === 201) {
+      alert("✅ Реєстрація успішна!");
+    } else {
+      alert("❌ Помилка: " + result.error);
+    }
+  });
+
+// Функція перевірки пароля на кількість символів і спецсимволи
+function validatePassword(password) {
+  const passwordError = document.getElementById("passwordError");
+  const specialChars = /[!@#$%^&*(),.?":{}|<>]/; // Regex для пошуку спецсимволів
+  let errorMessage = "";
+
+  if (password.length < 8) {
+    errorMessage = "Пароль має бути не менше 8 символів";
+  } else if (!specialChars.test(password)) {
+    // test() — це метод об'єкта RegExp, який перевіряє, чи рядок (password) містить хоча б один із вказаних символів. Оператор ! (логічне заперечення) інвертує результат
+    errorMessage = "Пароль має містити хоча б один спецсимвол";
+  }
+
+  if (errorMessage) {
+    passwordError.textContent = errorMessage;
+    passwordError.style.display = "block";
+    return false;
+  } else {
+    passwordError.style.display = "none";
+    return true;
+  }
+}
+
+document.getElementById("toggleUsers").addEventListener("click", async () => {
+  const dropdown = document.getElementById("userDropdown");
+
+  // Перемикаємо відображення дропдауна
+  dropdown.style.display =
+    dropdown.style.display === "block" ? "none" : "block";
+
+  // Якщо дропдаун відкривається завантажуємо користувачів
+  if (dropdown.style.display === "block") {
+    try {
+      const response = await fetch("http://localhost:3000/users");
+      console.log(response);
+      const users = await response.json();
+      const userList = document.getElementById("userList");
+      userList.innerHTML = ""; // Очищаємо попередній список
+
+      users.forEach((user) => {
+        const li = document.createElement("li");
+        li.textContent = `${user.name} - ${user.email}`;
+        userList.appendChild(li);
+      });
+    } catch (error) {
+      console.error("Помилка завантаження користувачів:", error);
+    }
+  }
+});
---
+++ server.js
@@ -0,0 +1,73 @@
+const express = require("express");
+const sqlite3 = require("sqlite3").verbose();
+const bodyParser = require("body-parser");
+const cors = require("cors");
+
+const app = express();
+const db = new sqlite3.Database("./database.db");
+
+app.use(cors({ origin: "http://127.0.0.1:5500" }));
+app.use(bodyParser.json());
+
+db.run(
+  `CREATE TABLE IF NOT EXISTS users (
+      id INTEGER PRIMARY KEY AUTOINCREMENT, 
+      name TEXT, 
+      email TEXT UNIQUE, 
+      password TEXT,
+      gender TEXT
+    )`
+);
+
+app.post("/register", (req, res) => {
+  const { name, email, password, gender } = req.body;
+
+  if (!name || !email || !password) {
+    return res.status(400).json({ error: "Всі поля обов'язкові" });
+  }
+
+  // Перевіряємо, чи є вже юзер із таким email
+  db.get("SELECT * FROM users WHERE email = ?", [email], (err, user) => {
+    if (err) {
+      console.log("Помилка пошуку користувача:", err.message);
+      return res.status(500).json({ error: "Помилка сервера" });
+    }
+
+    if (user) {
+      console.warn(`Email вже зайнятий: ${email}`);
+      return res
+        .status(409)
+        .json({ error: "Користувач з таким email вже існує" });
+    }
+
+    console.log(`Додаємо користувача: ${name} ${email}`);
+
+    // Якщо такого юзера ще немає, додаємо його в базу
+    db.run(
+      "INSERT INTO users (name, email, password, gender) VALUES (?, ?, ?, ?)",
+      [name, email, password, gender],
+      function (err) {
+        if (err) {
+          console.error("Помилка при додаванні користувача:", err.message);
+          return res.status(500).json({ error: err.message });
+        }
+        console.log(`Користувач доданий! ID: ${this.lastID}`);
+        res
+          .status(201)
+          .json({
+            message: "Користувач успішно зареєстрований!",
+            id: this.lastID,
+          });
+      }
+    );
+  });
+});
+
+app.get('/users', (req, res) => {
+    db.all(`SELECT name, email FROM users`, [], (err, rows) => {
+        if (err) return res.status(500).json({ error: err.message });
+        res.json(rows);
+    })
+})
+
+app.listen(3000, () => console.log("Server running on port 3000"));
